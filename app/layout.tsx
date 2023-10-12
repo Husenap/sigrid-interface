34312908b52c91a068f7decc5d701961d66f9064
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2023-10-12 23:56:05 +0200

    added logo in background

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import Providers from "@/components/providers/providers";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,10 +12,11 @@ export const metadata: Metadata = {
   description: "Take your labbhandledning to the next level! 🚀",
   openGraph: {
     title: "Sigridpunk",
+    description: "Take your labbhandledning to the next level! 🚀",
     siteName: "Sigridpunk",
     url: "https://sigrid-interface.vercel.app",
     images: {
-      url: "https://sigrid-interface.vercel.app/logo.png",
+      url: "https://sigrid-interface.vercel.app/logo.jpg",
       width: 1024,
       height: 1024,
       alt: "The AI are taking over!",
@@ -32,7 +35,17 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <Providers>
-          <main className="container">{children}</main>
+          <main className="container relative">
+            <div className="absolute top-0 right-0 h-screen -z-10 flex items-center">
+              <Image
+                src="/logo.png"
+                width={1024}
+                height={1024}
+                alt="Sigridpunk is taking over"
+              />
+            </div>
+            {children}
+          </main>
         </Providers>
       </body>
     </html>
