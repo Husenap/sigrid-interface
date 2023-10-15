6eab8a097d0875874a50450886c303026683dd07
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2023-10-16 00:44:20 +0200

    cleanup logs

--- app/monitor/actions.ts
+++ app/monitor/actions.ts
@@ -10,19 +10,17 @@ let cache: {
 
 export default async function getDatabase() {
   if (cache && Date.now() - cache.date < 10000) {
-    console.log('Returning cached data');
     return cache.database;
-  } else {
-    const res = await apiCall(['sigrid', 'monitor'], {});
-    const page = await res.text();
-    const database = await parse(page);
-
-    cache = {
-      database,
-      date: Date.now(),
-    };
-
-    console.log('Returning fresh data');
-    return database;
   }
+
+  const res = await apiCall(['sigrid', 'monitor'], {});
+  const page = await res.text();
+  const database = await parse(page);
+
+  cache = {
+    database,
+    date: Date.now(),
+  };
+
+  return database;
 }
