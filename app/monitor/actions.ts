bc92809b161282617cd67dd6340adc99a001e604
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2023-10-15 23:27:50 +0200

    updated getDatabase function

--- app/monitor/actions.ts
+++ app/monitor/actions.ts
@@ -1,11 +1,11 @@
 "use server";
 
 import { apiCall } from "@/lib/api";
-import parse from "@/lib/parser";
+import { parse } from "@/lib/parser";
 
 export default async function getDatabase() {
   const res = await apiCall(["sigrid", "monitor"], {});
-
   const page = await res.text();
-  return parse(page);
+  const database = await parse(page);
+  return database;
 }
