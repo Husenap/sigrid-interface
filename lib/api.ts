a1062e02a34043bd0621831aff882fd690ab334e
Hussein Taher <6496177+Husenap@users.noreply.github.com>
2023-10-16 14:49:30 +0200

    updated api

--- lib/api.ts
+++ lib/api.ts
@@ -1,13 +1,22 @@
+'use server';
+
+const BASE_URL = process.env.SIGRID_URL || '';
+
 export async function apiCall(path: Array<string>, params: any) {
-  const res = await fetch('/api', {
-    method: 'POST',
+  const apiUrl = [BASE_URL, ...path].join('/');
+  const searchParams = new URLSearchParams(params);
+  const url = `${apiUrl}?${searchParams}`;
+
+  const res = await fetch(url, {
+    method: 'GET',
     cache: 'no-cache',
-    body: JSON.stringify({ path, params }),
   });
 
   if (!res.ok) {
     throw new Error(await res.text());
   }
 
-  return await res.text();
+  const page = await res.text();
+
+  return page;
 }
